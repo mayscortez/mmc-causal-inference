adb70348d9e9e58bfd7aa7d0b2854f68f90d6b87
Mays Cortez <lovatic27@gmail.com>
2022-05-19 12:13:26 -0400

    new plots

--- Code-for-Plots/master_plots.py
+++ Code-for-Plots/master_plots.py
@@ -16,65 +16,58 @@ def main():
     x_label = ['ratio', 'tp', 'size']#['varying']
     x_var = ['ratio', 'p', 'n']#['beta']
     model = ['deg2','linear']#['deg']
-    # x_label = ['varying']
-    # x_var = ['beta']
+    #x_label = ['varying']
+    #x_var = ['beta']
     #model = ['deg']
     for b in model:
         for ind in range(len(x_var)):
             plot(graph,x_var[ind],x_label[ind],b)
 
 def plot(graph,x_var,x_label,model):
-
+    CRD_est_lin = ['Graph-Agnostic-num', 'Diff-Means-Stnd', 'Diff-Means-Frac-0.75', 'OLS-Prop', 'OLS-Num']
+    CRD_est_deg2 = ['Graph-Agnostic-num', 'Diff-Means-Stnd', 'Diff-Means-Frac-0.75', 'LeastSqs-Prop', 'LeastSqs-Num']
+    our_est = ['Graph-Agnostic-p', 'Graph-Agnostic-num', 'Graph-AgnosticVR', ]
     experiment = '-'+x_label+'-'+model
     print(experiment)
 
     # Create and save plots
     df = pd.read_csv(load_path+graph+experiment+'-full-data.csv')
 
+    """
     # Plot with all the estimators
     fig = plt.figure()
     ax = fig.add_subplot(111)
+    if model == 'linear':
+        newData = df.loc[df['Estimator'].isin(CRD_est_lin)]
+    else:
+        newData = df.loc[df['Estimator'].isin(CRD_est_deg2)]
 
-    sns.lineplot(x=x_var, y='Bias', hue='Estimator', style='Estimator', data=df, ci='sd', legend='brief', markers=True)
+    sns.lineplot(x=x_var, y='Bias', hue='Estimator', style='Estimator', data=newData, ci='sd', legend='brief', markers=True)
     ax.set_ylim(-1,1)
     ax.set_xlabel(x_var, fontsize = 12)
     ax.set_ylabel("Relative Bias", fontsize = 12)
-    ax.set_title('Performance of Estimator', fontsize=16)
+    ax.set_title('Performance of Estimators under CRD', fontsize=16)
     handles, labels = ax.get_legend_handles_labels()
     ax.legend(handles=handles, labels=labels)
 
-    plt.savefig(save_path+graph+experiment+'-all.pdf')
+    plt.savefig(save_path+graph+experiment+'-allCRD.pdf')
     plt.close()
+    """
 
-    # # Plot with our estimator and Least sqs
-    # fig = plt.figure()
-    # ax = fig.add_subplot(111)
+    # Plot with our the estimators
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
 
-    # sns.lineplot(x=x_var, y='Bias', hue='Estimator', style='Estimator', data=df.loc[df['Estimator'].isin(['Graph-Agnostic','LeastSqs-Prop','LeastSqs-Num'])], ci='sd', legend='brief', markers=True)
-    # ax.set_xlabel(x_var, fontsize = 12)
-    # ax.set_ylabel("Relative Bias", fontsize = 12)
-    # ax.set_title('Performance of Estimators', fontsize=16)
-    # handles, labels = ax.get_legend_handles_labels()
-    # ax.legend(handles=handles, labels=labels)
-    # plt.savefig(save_path+graph+experiment+'-oursAndLS.pdf')
-    # plt.close()
+    sns.lineplot(x=x_var, y='Bias', hue='Estimator', style='Estimator', data=df.loc[df['Estimator'].isin(our_est)], ci='sd', legend='brief', markers=True)
+    ax.set_ylim(-0.25,0.25)
+    ax.set_xlabel(x_var, fontsize = 12)
+    ax.set_ylabel("Relative Bias", fontsize = 12)
+    ax.set_title('Performance of our Estimators', fontsize=16)
+    handles, labels = ax.get_legend_handles_labels()
+    ax.legend(handles=handles, labels=labels)
 
-    # # Plot with our estimator and interpolation
-    # fig = plt.figure()
-    # ax = fig.add_subplot(111)
-
-    # sns.lineplot(x=x_var, y='Bias', hue='Estimator', style='Estimator', data=df.loc[df['Estimator'].isin(['Graph-Agnostic','Interp-Lin','Spline-Lin', 'Spline-Quad'])], ci='sd', legend='brief', markers=True)
-    # ax.set_xlabel(x_var, fontsize = 12)
-    # ax.set_ylabel("Relative Bias", fontsize = 12)
-    # ax.set_title('Performance of Estimators', fontsize=16)
-    # handles, labels = ax.get_legend_handles_labels()
-    # ax.legend(handles=handles, labels=labels)
-    # plt.savefig(save_path+graph+experiment+'-oursAndinterp.pdf')
-    # plt.close()
-
-    # # Plot with graph agnostic and both OLS estimators
-    # fig = plt.figure()
-    # ax = fig.add_subplot(111)
+    plt.savefig(save_path+graph+experiment+'-ours.pdf')
+    plt.close()
 
 
 if __name__ == "__main__":
