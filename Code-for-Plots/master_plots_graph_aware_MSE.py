4a9a23e56166d9fd1ca659b74415d75fbbb0b3ac
Mays Cortez <lovatic27@gmail.com>
2022-08-02 11:14:41 -0400

    Create master_plots_graph_aware_MSE.py

---
+++ Code-for-Plots/master_plots_graph_aware_MSE.py
@@ -0,0 +1,72 @@
+# Plot MSE
+
+# Setup
+from matplotlib import rcParams
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+load_path = 'outputFiles/graph_aware/'
+save_path = 'outputFiles/graph_aware/'
+
+def main():    
+    x_label = ['ratio', 'tp', 'size']
+    x_var = ['ratio', 'p', 'n']
+    x_plot = ['$r$', '$p$', '$n$']
+
+    graph = "er"
+    for beta in [1,2]:
+        title = ['$\\beta='+str(beta)+', n=15000, p=0.2$','$\\beta='+str(beta)+', n=15000, r=2$','$\\beta='+str(beta)+', p=0.2, r=2$']
+        est_names = ['SNIPE('+str(beta)+')', 'DM', 'DM($0.75$)', 'LS-Prop', 'LS-Num']
+        for ind in [0,1,2]:
+            plot(graph,x_var[ind],x_label[ind],'deg'+str(beta),x_plot[ind],title[ind],est_names)
+
+
+def plot(graph,x_var,x_label,model,x_plot,title,est_names,permute=False):
+    experiment = '-'+x_label+'-'+model
+    print(experiment)
+
+    # Create and save plots
+    df = pd.read_csv(load_path+graph+experiment+'-graph_aware.csv')
+    df = df.assign(Estimator = lambda df: df.Estimator.replace({'Graph-Aware':est_names[0], 'LeastSqs-Prop':est_names[3],'LeastSqs-Num':est_names[4],'Diff-Means-Stnd': est_names[1], 'Diff-Means-Frac-0.75':est_names[2]}))
+
+    # Uncomment line below for LaTeX font
+    plt.rc('text', usetex=True)
+    
+    # Plot with all the estimators
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    if model == 'deg1':
+        # Compute MSE as average of squared relative biases but exclude DM estimators from plot
+        df = df.loc[(df["Estimator"]!='DM') & (df['Estimator']!='DM($0.75$)')]
+        df["biassq"] = df["Bias"]**2
+        df2 = df.groupby([x_var,'Estimator']).agg('mean')
+
+        # Plot
+        sns.lineplot(x=x_var, y='biassq', hue='Estimator', style='Estimator', data=df2, palette = ['#2ba02c','#d62728','#9468bd'], legend='brief', dashes=[(1,1),(3,2,2,2),(4,1,1,1)], markers=['s','P','D'])
+        ax.set_ylim(0,0.05)
+    else:
+        # Compute MSE
+        df["biassq"] = df["Bias"]**2
+        df2 = df.groupby([x_var,'Estimator']).agg('mean')
+
+        # Plot
+        sns.lineplot(x=x_var, y='biassq', hue='Estimator', style='Estimator', data=df2, legend='brief', markers=True)
+        ax.set_ylim(0,0.5)
+    ax.set_xlabel(x_plot, fontsize = 18)
+    ax.set_ylabel("MSE", fontsize = 18)
+    ax.set_title(title, fontsize=20)
+    handles, labels = ax.get_legend_handles_labels()
+
+    if permute:
+        order = [0,3,4,1,2]
+        ax.legend(handles=[handles[i] for i in order], labels=[labels[i] for i in order], loc='upper right', fontsize = 14)
+    else:
+        ax.legend(handles=handles, labels=labels, loc='upper right', fontsize = 14)
+
+    plt.savefig(save_path+graph+experiment+'_graph_aware_MSE.pdf')
+    plt.close()
+
+if __name__ == "__main__":
+    main()
