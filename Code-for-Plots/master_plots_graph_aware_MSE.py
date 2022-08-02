cb0cf0ab2a4f7f4f451d0881900dbe5c2d63ed0b
Mays Cortez <lovatic27@gmail.com>
2022-08-02 15:16:29 -0400

    Update master_plots_graph_aware_MSE.py

--- Code-for-Plots/master_plots_graph_aware_MSE.py
+++ Code-for-Plots/master_plots_graph_aware_MSE.py
@@ -19,7 +19,7 @@ def main():
         title = ['$\\beta='+str(beta)+', n=15000, p=0.2$','$\\beta='+str(beta)+', n=15000, r=2$','$\\beta='+str(beta)+', p=0.2, r=2$']
         est_names = ['SNIPE('+str(beta)+')', 'DM', 'DM($0.75$)', 'LS-Prop', 'LS-Num']
         for ind in [0,1,2]:
-            plot(graph,x_var[ind],x_label[ind],'deg'+str(beta),x_plot[ind],title[ind],est_names)
+            plot(graph,x_var[ind],x_label[ind],'deg'+str(beta),x_plot[ind],title[ind],est_names,permute=True)
 
 
 def plot(graph,x_var,x_label,model,x_plot,title,est_names,permute=False):
@@ -30,29 +30,22 @@ def plot(graph,x_var,x_label,model,x_plot,title,est_names,permute=False):
     df = pd.read_csv(load_path+graph+experiment+'-graph_aware.csv')
     df = df.assign(Estimator = lambda df: df.Estimator.replace({'Graph-Aware':est_names[0], 'LeastSqs-Prop':est_names[3],'LeastSqs-Num':est_names[4],'Diff-Means-Stnd': est_names[1], 'Diff-Means-Frac-0.75':est_names[2]}))
 
+    df["biassq"] = df["Bias"]**2
+    df2 = df.groupby([x_var,'Estimator']).agg('mean')
+
     # Uncomment line below for LaTeX font
     plt.rc('text', usetex=True)
     
     # Plot with all the estimators
     fig = plt.figure()
     ax = fig.add_subplot(111)
+    
+    # '#2077b4' - blue; '#ff7f0e' - orange; '#2ba02c' - green; '#d62728' - red; '#9468bd' - purple
+    sns.lineplot(x=x_var, y='biassq', hue='Estimator', style='Estimator', data=df2, palette=['#ff7f0e', '#2ba02c', '#9468bd', '#d62728', '#2077b4'], legend='brief', markers=True)
 
     if model == 'deg1':
-        # Compute MSE as average of squared relative biases but exclude DM estimators from plot
-        df = df.loc[(df["Estimator"]!='DM') & (df['Estimator']!='DM($0.75$)')]
-        df["biassq"] = df["Bias"]**2
-        df2 = df.groupby([x_var,'Estimator']).agg('mean')
-
-        # Plot
-        sns.lineplot(x=x_var, y='biassq', hue='Estimator', style='Estimator', data=df2, palette = ['#2ba02c','#d62728','#9468bd'], legend='brief', dashes=[(1,1),(3,2,2,2),(4,1,1,1)], markers=['s','P','D'])
         ax.set_ylim(0,0.05)
     else:
-        # Compute MSE
-        df["biassq"] = df["Bias"]**2
-        df2 = df.groupby([x_var,'Estimator']).agg('mean')
-
-        # Plot
-        sns.lineplot(x=x_var, y='biassq', hue='Estimator', style='Estimator', data=df2, legend='brief', markers=True)
         ax.set_ylim(0,0.5)
     ax.set_xlabel(x_plot, fontsize = 18)
     ax.set_ylabel("MSE", fontsize = 18)
@@ -60,7 +53,7 @@ def plot(graph,x_var,x_label,model,x_plot,title,est_names,permute=False):
     handles, labels = ax.get_legend_handles_labels()
 
     if permute:
-        order = [0,3,4,1,2]
+        order = [4,0,1,3,2] #1 - DM75, 0- DM, 3-LSprop, 4-SNIpe, 2-LSnum
         ax.legend(handles=[handles[i] for i in order], labels=[labels[i] for i in order], loc='upper right', fontsize = 14)
     else:
         ax.legend(handles=handles, labels=labels, loc='upper right', fontsize = 14)
